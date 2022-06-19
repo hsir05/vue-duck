ed702a0e9b77d72cc35f9965ec8f09332506bcfe
haqsir@163.com <haqsir@163.com>
2022-06-19 11:53:46 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <Duck />
+</template>
+<script setup lang="ts">
+import Duck from './components/duck.vue'
+</script>
+
+
+
---
+++ src/components/duck.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="duck">
+
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+
+</style>
